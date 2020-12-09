9acbb38f1c724e69fe692e7633adc47d0ada5694
da-okazaki <da-okazaki@outlook.com>
2020-12-09 09:09:02 +0900

    [modify] Primarybutton 追加

--- src/components/UIKit/PrimaryButton.jsx
+++ src/components/UIKit/PrimaryButton.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import { Button } from '@material-ui/core'
-import { makeStyles } from '@material-ui/styles'
+import React from "react";
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles({
-  "button": {
+  button: {
+    borderRadius: 10,
     backgroundColor: "#3399FF",
-    color: "#000",
+    color: "#FFFFFF",
     fontSize: 16,
     height: 48,
     marginButton: 16,
     width: 256,
-  }
-})
+  },
+});
 
-const PrimaryButton = (props) => {
-
-  const classes = useStyles()
+const PrimaryButton = ({ label, onClick }) => {
+  const classes = useStyles();
 
   return (
     <div>
-      <Button className={classes.button} variant="contained" onClick={() => props.onClick()}>
-        {props.label}
+      <Button
+        className={classes.button}
+        variant="contained"
+        onClick={() => onClick()}
+      >
+        {label}
       </Button>
     </div>
   );
