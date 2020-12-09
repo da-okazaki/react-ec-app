e979e9ff9f0cecb23cdd78b0abca7d56a9187282
da-okazaki <da-okazaki@outlook.com>
2020-12-09 09:13:01 +0900

    [modify] Home変更

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { getUserId, getUserName } from '../reducks/users/selectors'
-import { useDispatch, useSelector } from 'react-redux';
-import { push } from 'connected-react-router';
+//import React, { useState, useEffect } from 'react';
+import * as React from "react";
+//import { useState, useEffect } from "react";
+import { getUserId, getUserName } from "../reducks/users/selectors";
+import { useDispatch, useSelector } from "react-redux";
+import { push } from "connected-react-router";
 
 const Home = (props) => {
   const dispatch = useDispatch();
@@ -11,12 +13,11 @@ const Home = (props) => {
   return (
     <div>
       <h2>Home</h2>
-      <button onClick={() => dispatch(push('/login'))}>
-        ログインへ
-      </button>
+      <button onClick={() => dispatch(push("/login"))}>ログインへ</button>
       <p>uid:{uid}</p>
       <p>username:{username}</p>
     </div>
   );
 };
+
 export default Home;
